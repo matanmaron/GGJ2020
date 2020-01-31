2451c591b34963e9bee84104fc8130ea111ff99f
elya <eliaa1to6@gmail.com>
2020-01-31 15:19:23 +0200

    c2
    c2

--- Assets/Scripts/menu/menuController.cs
+++ Assets/Scripts/menu/menuController.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class menuController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Button start_button;
+    public Button credit_button;
+    public Button exit_button;
     void Start()
     {
+        start_button.onClick.AddListener(onClickStart);
+        credit_button.onClick.AddListener(onClickCredit);
+        exit_button.onClick.AddListener(onClickExit);
         
     }
 
@@ -14,5 +21,20 @@ public class menuController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void onClickStart()
+    {
+        SceneManager.LoadScene("mainScene");
+    }
+
+    void onClickCredit()
+    {
+        
+    }
+
+    void onClickExit()
+    {
+        Application.Quit();
     }
 }
