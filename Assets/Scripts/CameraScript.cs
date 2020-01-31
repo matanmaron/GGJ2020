10670794e8e732ba4009ccab53a171f2a0e94293
Matan Maron <matan.maron@pixigaming.com>
2020-01-31 11:40:18 +0200

    add camera control, game manager, and player control

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField] private bool IsDebug = false;
+    [SerializeField] private float SensitivityX = 1;
+    [SerializeField] private float SensitivityY = 1;
+    [SerializeField] private float MinimumY = -20;
+    [SerializeField] private float MaximumY = 40;
+    private PlayerControlScript _target;
+    private float _rotationY;
+    private float _rotationX;
+
+    void Start()
+    {
+        var player = GameObject.Find("Player");
+        if (IsDebug && player == null)
+        {
+            Debug.Log("cannot find player in scene !");
+        }
+
+        _target = player.GetComponent<PlayerControlScript>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    void LateUpdate()
+    {
+        if (IsDebug && _target == null)
+        { 
+            Debug.Log("cannot find playercontrolscript in player !");
+        }
+        _rotationX += Input.GetAxis("Mouse X") * SensitivityX;
+        _rotationY += Input.GetAxis("Mouse Y") * SensitivityY;
+        _rotationY = Mathf.Clamp(_rotationY, MinimumY, MaximumY);
+        transform.localEulerAngles = new Vector3(-_rotationY, _rotationX, 0);
+        transform.position = _target.transform.position;
+    }
+}
---
+++ Assets/Scripts/PlayerControlScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlScript : MonoBehaviour
+{
+    [SerializeField] private bool IsDebug = false;
+    [SerializeField] private float Speed = 15;
+    [SerializeField] private float SensitivityX = 1;
+    private float _rotationX;
+    private CharacterController _controller;
+    private Animator _animator;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        if (IsDebug && _controller == null)
+        {
+            Debug.Log("cannot find character controller");
+        }
+        _animator = GetComponent<Animator>();
+        if (IsDebug && _animator == null)
+        {
+            Debug.Log("cannot find character animator");
+        }
+    }
+    
+    void LateUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+         void LateUpdate()
+            {
+                Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+                _controller.Move(move * (Time.deltaTime * Speed));
+    
+                _rotationX += Input.GetAxis("Mouse X") * SensitivityX;
+                transform.localEulerAngles = new Vector3(0, _rotationX, 0);
+    
+                if (move != Vector3.zero || _rotationX != 0)
+                {
+                    WalkAnim();
+                }
+                _rotationX += Input.GetAxis("Mouse X") * SensitivityX;
+                transform.localEulerAngles = new Vector3(0, _rotationX, 0);
+            }
+    }
+
+    private void WalkAnim()
+    {
+        //TODO
+    }
+}
