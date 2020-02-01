e4b46520b70c7ef2a40742ed06140857030ab2dc
Matan Maron <matan.maron@pixigaming.com>
2020-02-01 15:47:24 +0200

    main music

---
+++ Assets/Game/Scripts/MainMusicScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMusicScript : MonoBehaviour
+{
+    [SerializeField] private AudioSource music;
+
+    void Start()
+    {
+        music.Play();
+    }
+}
