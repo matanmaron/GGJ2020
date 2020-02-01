87891e93c039f1ba535c18f91dca695154534b05
Matan Maron <matan.maron@pixigaming.com>
2020-02-01 15:44:00 +0200

    music menu

---
+++ Assets/Game/Scripts/MenuMusicScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusicScript : MonoBehaviour
+{
+    [SerializeField] AudioSource music;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayMusic());
+    }
+
+    public IEnumerator PlayMusic()
+    {
+        yield return new WaitForSeconds(10);
+        music.Play();
+    }
+}
